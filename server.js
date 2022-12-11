1bf12f99e3e98fcb5b5a9a325975fb2e246c6670
Adel <adel.amirali@outlook.com>
2022-12-11 19:15:31 +0100

    Backend - server.js - setup mongoose to connect to the DB

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 // destructure environment variables.
-const {PORT} = dotenv.config().parsed;
+const {PORT, DB_USER, DB_PASS, DB_HOST, DB_NAME} = dotenv.config().parsed;
+
+//connect to mongoDB using mongoose.
+mongoose
+    .set('strictQuery', false)
+    .connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log('connected to the DB.');
+    })
+    .catch(err => {
+        console.log('can\'t connect to the DB.', err.message);
+    });
 
 // initialize server.
 const app = express();
