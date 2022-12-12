39085cc24be5e659b03880b299dd602e060cae32
Adel <adel.amirali@outlook.com>
2022-12-12 17:40:54 +0100

    Frontend - icons & fonts - add icons & fonts to header

--- client/src/components/Header/Public/Header.jsx
+++ client/src/components/Header/Public/Header.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+import {ReactComponent as LoginIcon} from '../../../imgs/icons/login_FILL0_wght700_GRAD200_opsz24.svg';
+import {ReactComponent as RegisterIcon} from '../../../imgs/icons/app_registration_FILL0_wght700_GRAD200_opsz24.svg';
+
+
 import styles from './Header.module.css';
 
 export default function Header () {
 
     return (<>
-        Public Header
+        <header className={styles.Header}>
+            <div className={styles.Logo}>
+                <Link to="/">
+                    real time chat app
+                </Link>
+            </div>
+            <div className={styles.Icons}>
+                <Link>
+                    <LoginIcon />
+                    
+                </Link>
+                <Link>
+                    <RegisterIcon />
+                </Link>
+            </div>
+        </header>
     </>);
 }
