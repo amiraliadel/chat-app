4c6ae02ecf2608e819710a5ce589f14639dc08a0
Adel <adel.amirali@outlook.com>
2022-12-13 09:54:44 +0100

    Frontend - users page - create users page - add users path to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
+import {AuthProvider} from './contexts/AuthContext';
 import Layout from './components/Layout/Layout';
 import Home from './routes/HomePage/Home';
 import Auth from './routes/AuthPage/Auth';
+import Users from './routes/UsersPage/Users';
 import Login from './routes/LoginPage/Login';
 import Register from './routes/RegisterPage/Register';
 import NoMatch from './routes/404/NoMatch';
@@ -19,6 +20,7 @@ function App() {
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Register/>}/>
           <Route path='/auth' element={<Auth/>}/>
+          <Route path='/users' element={<Users/>}/>
           <Route path='/*' element={<NoMatch/>}/>
         </Route>
       </Routes>
---
+++ client/src/routes/UsersPage/Users.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from './Users.module.css';
+
+export default function Users () {
+
+    return (<>
+        Users
+    </>);
+}
