405f7aed0f94206799b6158f8178a5ccd4582e9b
Adel <adel.amirali@outlook.com>
2023-01-05 18:44:28 +0100

    Frontend - Login - Context - create handleLogin function in AuthContext - add it to Login page

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -4,10 +4,21 @@ const AuthContext = createContext();
 
 function AuthProvider ({children}) {
     const [loggedIn, setLoggedIn] = useState(false);
+    const [username, setUsername] = useState('');
+
+    // handle login.
+    const handleLogin = (isLoggedIn, username) => {
+        setLoggedIn(isLoggedIn);
+        isLoggedIn ? setUsername(username) : setUsername('');
+    }
 
     return (
         <AuthContext.Provider 
-            value={{loggedIn}}>
+            value={{
+                loggedIn,
+                username,
+                handleLogin
+                }}>
             {children}
         </AuthContext.Provider>
     );
--- client/src/routes/LoginPage/Login.jsx
+++ client/src/routes/LoginPage/Login.jsx
@@ -1,12 +1,35 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, {useState, useContext} from "react";
+import axios from "axios";
+import {Link, useNavigate} from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import Log from "../../components/Shared/Log/Log";
 import styles from './Login.module.css';
 
 export default function Login () {
+    const [errors, setErrors] = useState();
+    const {handleLogin} = useContext(AuthContext);
+    const navigate = useNavigate();
 
+    const loginUser = async event => {
+        event.preventDefault();
 
-    const loginUser = () => {
-
+        const formData = new FormData(event.target);
+        const data = {
+            email: formData.get('email'),
+            password: formData.get('password')
+        };
+        try {
+            const res = await axios.post('http://localhost:3003/users/login', data, {
+                withCredentials: true
+            });
+            if (res.data.success) {
+                setErrors('');
+                handleLogin(true, res.data.username);
+                // navigate('/');
+            }
+        } catch (err) {
+            setErrors(err.response.data.server_message);
+        }
     }
 
     return (<>
@@ -29,7 +52,11 @@ export default function Login () {
                     <button type="submit">submit</button>
                     <p>need to register? <Link to="/auth/register">sign up</Link></p>
                 </div>
-                
+                {(() => {
+                    if (errors) {
+                        return (errors.map(err => <Log msg={err.msg} key={err.msg} />));
+                    }
+                })()}
             </form>
         </div>
     </>);
