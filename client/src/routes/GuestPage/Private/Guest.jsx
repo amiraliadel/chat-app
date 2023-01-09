a423ea35a0216f71fb2dc7ac7a8fa01be9ba5457
Adel <adel.amirali@outlook.com>
2023-01-09 15:36:09 +0100

    Frontend - Guest page - import Guidance in private Guest page

--- client/src/routes/GuestPage/Private/Guest.jsx
+++ client/src/routes/GuestPage/Private/Guest.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Guidance from "../../../components/Shared/Guidance/Guidance";
 import styles from './Guest.module.css'
 
 export default function Guest () {
 
     return (<>
-        private guest
+        <div></div>
+        <Guidance msg="you are currently logged in. if you want to use the app as a guest, please logout first."/>
     </>);
 }
