b8548c44c3ed629f624324da75a568fa9fab364c
Adel <adel.amirali@outlook.com>
2023-01-10 09:55:49 +0100

    Frontend - Guest page - setup initial functionality to join a room

--- client/src/routes/GuestPage/Public/Guest.jsx
+++ client/src/routes/GuestPage/Public/Guest.jsx
@@ -3,31 +3,63 @@ import {Manager} from 'socket.io-client';
 import styles from './Guest.module.css'
 
 const manager = new Manager('http://localhost:3003', {withCredentials: true});
-const socket = manager.socket('/');
+const socket = manager.socket('/guest');
 
 export default function Guest () {
     const [userId, setUserId] = useState('');
+    const [room, setRoom] = useState('');
 
     socket.on('error', err => {
         console.log(err);
       });
 
-    socket.on('connect', () => {
-        console.log('connected');
-    });
-    console.log(socket.id);
+
+    
     useEffect(() => {
-        setUserId('usdlfjnsfjsfvsld');
-    });
+        socket.on('connect', () => {
+            console.log('connected');
+            setUserId(socket.id);
+        });
+        
+    }, []);
+
+    const joinRoom = async (event) => {
+        event.preventDefault();
+        const data = {
+            room: room,
+            userId: userId
+        };
+        try {
+            socket.emit('joinRoom', data);
+            
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+
+    const handleInput = event => {
+        setRoom(event.target.value);
+    }
+
+    const clearInput = () => {
+        setRoom('');
+    }
 
     return (<>
         <div className={styles.Sidebar}>
-            <form>
+            <form onSubmit={joinRoom}>
                 <label htmlFor="userId">
                     your id: <span id="userId">{userId}</span>
                 </label>
                 <label htmlFor="room">
-                    <input type="text" id="room" placeholder="enter an ID to join"/>
+                    <input 
+                    type="text" 
+                    id="room" 
+                    placeholder="enter an ID to join"
+                    value={room}
+                    onChange={handleInput}
+                    onClick={clearInput}
+                    />
                 </label>
                 <button type="submit">join</button>
             </form>
