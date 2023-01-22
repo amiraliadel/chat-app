c997103b6d69555ed91188c661fca938f13e1de3
Adel <adel.amirali@outlook.com>
2023-01-22 10:59:38 +0100

    Backend => Controllers => logout - change secure: true & sameSite: None for cookie header

--- controllers/users/logout.js
+++ controllers/users/logout.js
@@ -1,6 +1,6 @@
 export default async (req, res) => {
     return res
     .status(200)
-    .clearCookie('jwtToken', {httpOnly: true, secure: false, sameSite: 'lax'})
+    .clearCookie('jwtToken', {httpOnly: true, secure: true, sameSite: 'None'})
     .json({server_message: 'you are successfully logged out.', success: true});
 }
